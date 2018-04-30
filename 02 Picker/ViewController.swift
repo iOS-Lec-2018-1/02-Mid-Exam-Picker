260b00b9fc173d273851d81fde2e4aecbd872d54
Jonghyun Kim <jhkim@dit.ac.kr>
2018-04-30 10:41:25 +0900

    mid exam 02

--- 02 Picker/ViewController.swift
+++ 02 Picker/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    var color = ["흰색", "초록색", "노란색"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return color.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return color[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch String(row) {
+        case "1":
+            view.backgroundColor = UIColor.green
+        case "2":
+            view.backgroundColor = UIColor.yellow
+        default:
+            view.backgroundColor = UIColor.white
+        }
+    }
+    
 
+    @IBOutlet weak var myPickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myPickerView.delegate = self
+        myPickerView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
